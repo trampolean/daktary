3568b227256a98ad92455f570a0ef03d01568d6d
pointbar <stephane.langlois@scopyleft.fr>
2016-03-19 19:03:25 +0100

    Add comments to injectHTML

--- viewer.js
+++ viewer.js
@@ -1,3 +1,9 @@
+  /**
+   * Inject HTML code in #contribution tag.
+   *
+   * @param {String} An HTML string reprsenting a contribution.
+   *
+   */
 const injectHTML = (content) => {
   document.querySelector('#contribution').innerHTML = content
 }
