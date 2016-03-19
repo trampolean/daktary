bdf5ce6dd3674fdba9e61f40226b1a4bb910e3ef
pointbar <stephane.langlois@scopyleft.fr>
2016-03-19 19:03:56 +0100

    Write test for injectHTML

--- tests/test.viewer.js
+++ tests/test.viewer.js
@@ -1,7 +1,9 @@
 describe('Viewer', function() {
   describe('#injectHTML()', function() {
-    it('should return true', function() {
-      expect({ a: 'b' }).to.eql({ a: 'b' })
+    it('should inject text: "A contribution" in mocha test page', function() {
+      injectHTML('<p>A contribution</p>')
+      expect(document.querySelector('#contribution').textContent)
+        .to.eql('A contribution')
     })
   })
 })
