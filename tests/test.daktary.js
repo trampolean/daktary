817e7ab2ce10a555f3355d047ec2476159db572d
pointbar <stephane.langlois@scopyleft.fr>
2016-03-29 00:00:11 +0200

    Replace functions by arrows functions

--- tests/test.daktary.js
+++ tests/test.daktary.js
@@ -1,17 +1,17 @@
-describe('Daktary', function() {
-  describe('#GhUrl', function() {
-    it('should create a GitHubUrl object', function() {
+describe('Daktary', () =>
+  describe('#GhUrl', () => {
+    it('should create a GitHubUrl object', () => {
       const noValidGhUrl = new GithubUrl('#NotAnUrl')
       expect(noValidGhUrl).to.be.an('object')
     })
-    it('should check a GitHubUrl with a invalid url', function() {
+    it('should check a GitHubUrl with a invalid url', () => {
       const noValidGhUrl = new GithubUrl('#NotAnUrl')
       expect(noValidGhUrl.isValid).to.be(false)
     })
-    it('should check a GitHubUrl with a valid url', function() {
+    it('should check a GitHubUrl with a valid url', () => {
       const validGhUrl = new GithubUrl(
         'daktary-team/contributions/blob/master/créer_un_depôt_github.md')
       expect(validGhUrl.isValid).to.be(true)
     })
   })
-})
+)
