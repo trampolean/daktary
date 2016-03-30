d6cb2a2935124103df50283cf839a5bebc97aea2
pointbar <stephane.langlois@scopyleft.fr>
2016-03-30 23:59:44 +0200

    Test to check github tree and blob url

--- tests/test.daktary.js
+++ tests/test.daktary.js
@@ -8,10 +8,15 @@
       const noValidGhUrl = new GithubUrl('#NotAnUrl')
       expect(noValidGhUrl.isValid).to.be(false)
     })
-    it('should check a GitHubUrl with a valid url', () => {
+    it('should check a GitHubUrl with a blob valid url', () => {
       const validGhUrl = new GithubUrl(
         'daktary-team/contributions/blob/master/créer_un_depôt_github.md')
-      expect(validGhUrl.isValid).to.be(true)
+      expect(validGhUrl.checkGhBlob()).to.be(true)
+    })
+    it('should check a GitHubUrl with a valid tree url', () => {
+      const validGhUrl = new GithubUrl(
+        'daktary-team/contributions/tree/master/toast/recipes')
+      expect(validGhUrl.checkGhTree()).to.be(true)
     })
     it('should generate an API GitHub url', () => {
       const validGhUrl = new GithubUrl('owner/repo/blob/master/file.md')
