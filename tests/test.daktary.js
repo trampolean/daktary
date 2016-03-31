52530b6ed07a7a03378dadc99f17331242b8ed4f
pointbar <stephane.langlois@scopyleft.fr>
2016-03-31 15:20:56 +0200

    Add tests for components: breadcrumb, repositories, ressources

--- tests/test.daktary.js
+++ tests/test.daktary.js
@@ -1,43 +1,42 @@
-
-  describe('#GhUrl', () => {
-    it('should create a GitHubUrl object', () => {
-      const noValidGhUrl = new GithubUrl('#NotAnUrl')
-      expect(noValidGhUrl).to.be.an('object')
-    })
-    it('should check a GitHubUrl with a invalid url', () => {
-      const noValidGhUrl = new GithubUrl('#NotAnUrl')
-      expect(noValidGhUrl.isValid).to.be(false)
-    })
-    it('should check a GitHubUrl with a blob valid url', () => {
-      const validGhUrl = new GithubUrl(
-        'daktary-team/contributions/blob/master/créer_un_depôt_github.md')
-      expect(validGhUrl.checkGhBlob()).to.be(true)
-    })
-    it('should check a GitHubUrl with a valid tree url', () => {
-      const validGhUrl = new GithubUrl(
-        'daktary-team/contributions/tree/master/toast/recipes')
-      expect(validGhUrl.checkGhTree()).to.be(true)
-    })
-    it('should generate an API GitHub url', () => {
-      const validGhUrl = new GithubUrl('owner/repo/blob/master/file.md')
-      expect(validGhUrl.toGhApiUrl().startsWith(
-        'https://api.github.com/repos/owner/repo/contents/' +
-        'file.md?ref=master&client_id='
-      )).to.be(true)
+describe('#GhUrl', () => {
+  it('should create a GitHubUrl object', () => {
+    const noValidGhUrl = new GithubUrl('#NotAnUrl')
+    expect(noValidGhUrl).to.be.an('object')
+  })
+  it('should check a GitHubUrl with a invalid url', () => {
+    const noValidGhUrl = new GithubUrl('#NotAnUrl')
+    expect(noValidGhUrl.isValid).to.be(false)
+  })
+  it('should check a GitHubUrl with a blob valid url', () => {
+    const validGhUrl = new GithubUrl(
+      'daktary-team/contributions/blob/master/créer_un_depôt_github.md')
+    expect(validGhUrl.checkGhBlob()).to.be(true)
+  })
+  it('should check a GitHubUrl with a valid tree url', () => {
+    const validGhUrl = new GithubUrl(
+      'daktary-team/contributions/tree/master/toast/recipes')
+    expect(validGhUrl.checkGhTree()).to.be(true)
+  })
+  it('should generate an API GitHub url', () => {
+    const validGhUrl = new GithubUrl('owner/repo/blob/master/file.md')
+    expect(validGhUrl.toGhApiUrl().startsWith(
+      'https://api.github.com/repos/owner/repo/contents/' +
+      'file.md?ref=master&client_id='
+    )).to.be(true)
+  })
+})
+describe('#GithubBlob', () => {
+  it('should load a ressource with a local url', (done) => {
+    const keys = {id: atob(GH_ID), secret: atob(GH_SECRET)}
+    const localUrl =
+      new GithubBlob('daktary-team/daktary-team/blob/master/README.md')
+    const ghApiUrl =
+      `https://api.github.com/repos/daktary-team/daktary-team/` +
+      `contents/README.md?ref=master&client_id=${keys.id}` +
+      `&client_secret=${keys.secret}`
+    localUrl.loadGhContrib(ghApiUrl, (html) => {
+      expect(html).to.contain('daktary')
+      done()
     })
   })
-  describe('#GithubBlob', () => {
-    it('should load a ressource with a local url', (done) => {
-      const keys = {id: atob(GH_ID), secret: atob(GH_SECRET)}
-      const localUrl =
-        new GithubBlob('daktary-team/daktary-team/blob/master/README.md')
-      const ghApiUrl =
-        `https://api.github.com/repos/daktary-team/daktary-team/` +
-        `contents/README.md?ref=master&client_id=${keys.id}` +
-        `&client_secret=${keys.secret}`
-      localUrl.loadGhContrib(ghApiUrl, (html) => {
-        expect(html).to.contain('daktary')
-        done()
-      })
-    })
-  })
+})
