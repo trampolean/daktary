47c091268728b5f7ef1bcebe229762837581d319
pointbar <stephane.langlois@scopyleft.fr>
2016-04-13 13:33:14 +0200

    Write explicit parameters for template

--- component-repositories.js
+++ component-repositories.js
@@ -9,16 +9,16 @@
  * @result {String} A string representing an html list.
  */
 const tplRepositories = (data) =>
-  data.repositories.map(elt =>
+  data.repositories.map(({name, url, git_url}) =>
     `<article class="gh-list-item gh-type-repo">
-       <h2 class="gh-list-title"><a href="${elt.url}">${elt.name}</a></h2>
+       <h2 class="gh-list-title"><a href="${url}">${name}</a></h2>
        <div class="gh-list-meta">
          <p>Mis à jour le : 02/02/16</p>
          <p>Créé par : <a href="">pntbr</a> / Contributeurs les plus actifs :
            <a href="">pntbr</a> / <a href="">wolffgang</a>
          </p>
          <p>
-           <a href="${elt.git_url}">Voir sur Github</a>
+           <a href="${git_url}">Voir sur Github</a>
          </p>
        </div>
        <!--si <image--></image-->
@@ -27,7 +27,7 @@ const tplRepositories = (data) =>
        <p class="gh-list-excerpt">Le début de la fiche qui parle de ...</p>
        <a class="gh-list-readmore"
           title="Lire la suite de la fiche Titre de la fiche"
-          href="${elt.url}">Lire la fiche</a>
+          href="${url}">Lire la fiche</a>
      </article>`).join('\n')
 /**
  * Create data for Github repositories with a Github tree Url.
@@ -40,11 +40,11 @@ const dataRepositories = ({owner}, callback) => {
   fetch(apiUrl, {headers: {Accept: 'application/vnd.github.v3.html'}})
     .then(response => response.json())
     .then(json => {
-      const repositories = json.map(elt => ({
-        name: elt.name,
-        type: elt.type,
-        git_url: elt.html_url,
-        url: `/#${elt.html_url.match(/^https:\/\/github.com\/(.*)/)[1]}`
+      const repositories = json.map(({name, type, html_url}) => ({
+        name: name,
+        type: type,
+        git_url: html_url,
+        url: `/#${html_url.match(/^https:\/\/github.com\/(.*)/)[1]}`
       }))
       callback(repositories)
     })
