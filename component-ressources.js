4115787bc67d8af5c4593f2b6d8d3630353fb402
pointbar <stephane.langlois@scopyleft.fr>
2016-04-13 13:21:59 +0200

    Refactor rzssources

--- component-ressources.js
+++ component-ressources.js
@@ -9,17 +9,17 @@
  * @result {String} A string representing an html list.
  */
 const tplRessources = (data) =>
-  data.ressources.map(elt =>
-    `<article class="gh-list-item gh-type-${elt.type}">
-       <h2 class="gh-list-title"><a href="${elt.url}">${elt.name}</a></h2>
+  data.ressources.map(({name, type, url, prose_url, git_url}) =>
+    `<article class="gh-list-item gh-type-${type}">
+       <h2 class="gh-list-title"><a href="${url}">${name}</a></h2>
        <div class="gh-list-meta">
          <p>Mis à jour le : 02/02/16</p>
          <p>Créé par : <a href="">pntbr</a> / Contributeurs les plus actifs :
            <a href="">pntbr</a> / <a href="">wolffgang</a>
          </p>
          <p>
-           ${elt.type === 'file' ? `<a href="${elt.prose_url}">Editer la fiche</a> - ` : ''}
-           <a href="${elt.git_url}">Voir sur Github</a>
+           ${type === 'file' ? `<a href="${prose_url}">Editer la fiche</a> - ` : ''}
+           <a href="${git_url}">Voir sur Github</a>
          </p>
        </div>
        <!--si <image--></image-->
@@ -28,7 +28,7 @@ const tplRessources = (data) =>
        <p class="gh-list-excerpt">Le début de la fiche qui parle de ...</p>
        <a class="gh-list-readmore"
           title="Lire la suite de la fiche Titre de la fiche"
-          href="${elt.url}">Lire la fiche</a>
+          href="${url}">Lire la fiche</a>
      </article>`).join('\n')
 /**
  * Create data for Github ressources with a Github tree Url.
@@ -39,15 +39,15 @@ const tplRessources = (data) =>
 const dataRessources = ({owner, repo, branch, path}, callback) => {
   const params = {owner: owner, repo: repo, branch: branch, path: path}
   const apiUrl = new GithubUrl(params).toGhApiUrl()
-  fetch(apiUrl, {headers: {Accept: 'application/vnd.github.v3.html'}})
+  fetch(apiUrl, {headers: {Accept: 'application/vnd.github.v3'}})
     .then(response => response.json())
     .then(json => {
-      const ressources = json.map(elt => ({
-        name: elt.name,
-        type: elt.type,
-        prose_url: `http://prose.io/#${elt.html_url.match(/^https:\/\/github.com\/(.*)/)[1]}`.replace('blob', 'edit'),
-        git_url: elt.html_url,
-        url: `/#${elt.html_url.match(/^https:\/\/github.com\/(.*)/)[1]}`
+      const ressources = json.map(({name, type, html_url}) => ({
+        name: name,
+        type: type,
+        prose_url: `http://prose.io/#${html_url.match(/^https:\/\/github.com\/(.*)/)[1]}`.replace('blob', 'edit'),
+        git_url: html_url,
+        url: `/#${html_url.match(/^https:\/\/github.com\/(.*)/)[1]}`
       }))
       callback(ressources)
     })
@@ -64,13 +64,14 @@ const dataSearch = ({owner, repo, branch, path}, search, callback) => {
   fetch(apiUrl, {headers: {Accept: 'application/vnd.github.v3.html'}})
     .then(response => response.json())
     .then(json => {
-      const ressources = json.items.map(elt => ({
-        name: elt.name,
-        type: elt.type,
-        prose_url: `http://prose.io/#${elt.html_url.match(/^https:\/\/github.com\/(.*)/)[1]}`.replace('blob', 'edit'),
-        git_url: elt.html_url,
-        url: `/#${elt.repository.full_name}/blob/master/${elt.path}`
-      }))
+      const ressources = json.items.map(
+        ({name, type, path, html_url, repository}) => ({
+          name: name,
+          type: type,
+          prose_url: `http://prose.io/#${html_url.match(/^https:\/\/github.com\/(.*)/)[1]}`.replace('blob', 'edit'),
+          git_url: html_url,
+          url: `/#${repository.full_name}/blob/master/${path}`
+        }))
       callback(ressources)
     })
 }
