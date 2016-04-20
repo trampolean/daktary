22af2bf1535eeee174d1cc3b2e839b536a600922
pointbar <stephane.langlois@scopyleft.fr>
2016-04-20 15:29:18 +0200

    Adapt router with templater

--- routes.js
+++ routes.js
@@ -1,19 +1,23 @@
 // Create a router
 const router = new Router()
 
+router.route('search/code', function () {
+  this.currentRoute = 'search'
+  layout.searchList.render()
+})
 router.route(':owner/:repo/blob/:branch/:path(.*)', function () {
   this.currentRoute = 'blob'
-  this.injectLayout = injectLayoutViewer
+  layout.viewer.render()
 })
 router.route(':owner/:repo/tree/:branch/:path(.*)?', function () {
   this.currentRoute = 'tree'
-  this.injectLayout = injectLayoutRepos
+  layout.folders.render()
 })
 router.route(':owner/:repo', function () {
   this.currentRoute = 'list'
-  this.injectLayout = injectLayoutRepos
+  layout.folders.render()
 })
 router.route(':owner', function () {
   this.currentRoute = 'repos'
-  this.injectLayout = injectLayoutRepos
+  layout.repos.render()
 })
