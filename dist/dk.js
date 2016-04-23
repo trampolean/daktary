22f51a58136db5faa4a945f87954e1231fd36de4
pointbar <stephane.langlois@scopyleft.fr>
2016-04-23 12:29:43 +0200

    Optimize files

--- dist/dk.js
+++ dist/dk.js
@@ -1,3 +1,28 @@
+'use strict';
+
+var GH_SECRET = 'M2NmYjI1YmNlOWE4MGFjN2E2NzIxZTg5YzkwMGVhZjM5NzEwN2Y2MA==';
+var GH_ID = 'NGEzOWM4YzE4NjA3NDkxNWU1NDY=';
+
+var CREWS = { crews: [{ title: 'Bienvenue sur multiBàO',
+    label: 'Accueil Multibao',
+    owner: 'multibao'
+  }, {
+    title: 'Réseau Transition BE',
+    label: 'Réseau Transition BE',
+    owner: 'reseautransitionwb'
+  }, {
+    title: 'Réseau Coop-tic',
+    label: 'Coop-TIC',
+    owner: 'supagroflorac'
+  }, {
+    title: 'Traducteurs agiles',
+    label: 'Traducteurs agiles',
+    owner: 'les-traducteurs-agiles'
+  }, {
+    title: 'Onpassealacte',
+    label: 'Onpassealacte',
+    owner: 'onpassealacte'
+  }] };
 /**
    * An object to manage Github url.
    *
