8d2159b5f9268efaebf83c805d10781a78d4d59e
newick <erick@entre-quote.com>
2016-05-03 11:40:10 +0200

    removed crews everywhere but home

---
+++ src/layout-home.js
@@ -0,0 +1,18 @@
+{
+  layout.create('home')
+  layout.home.html(`
+  <header class="clearfix">
+    <h1>multiBàO</h1>
+    <div id="search-engine-wrapper" class="search-engine-wrapper" data-template="search">
+    </div>
+  </header>
+  <main>
+    <section class="home-intro">
+        <h2>Partager en équipe et au monde ses apprentissages sur le faire ensemble</h2>
+        <a href="">Commencer ici</a>
+        <a href="">Guide d'utilisation</a>
+    </section>
+    <section id="gh-crew-list" data-template="crews">
+    </section>
+  </main>`)
+}
