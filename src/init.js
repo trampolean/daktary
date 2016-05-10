bc08c2fd254fcec74d8ee980bdfb739f83c30e0b
pointbar <stephane.langlois@scopyleft.fr>
2016-05-10 21:56:21 +0200

    Try simple SPA approach

--- src/init.js
+++ src/init.js
@@ -1,9 +1,14 @@
-window.addEventListener('hashchange', () => window.location.reload(true))
+window.addEventListener('hashchange', () => {
+  const ghUrl = window.location.toString().split('#')[1]
+  router.go(ghUrl)
+})
+
 window.addEventListener('load', () => {
   const ghUrl = window.location.toString().split('#')[1]
   router.go(ghUrl)
   if (router.isNoRoute()) {
     window.location = './404.html'
+    window.location.reload(true)
   }
 })
 const template = new Template()
