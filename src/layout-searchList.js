4ec295fb5f41e34463e21d9ca3866d02511f436a
newick <erick@entre-quote.com>
2016-05-26 15:54:05 +0200

    added search result infos

--- src/layout-searchList.js
+++ src/layout-searchList.js
@@ -11,6 +11,12 @@
     </div>
   </header>
   <main>
+    <section class="search-result search-result-blank">
+    il n'y a pas de résultat pour la recherche <span>agilité</span> dans le repo <a href=""> Super repo de démo</a>
+    </section>
+    <section class="search-result">
+      <span>3</span> résultat(s) pour la recherche <span>agilité</span> dans le repo <a href=""> Super repo de démo</a>
+    </section>
     <section id="gh-list" class="gh-list" data-template="searchList">
     </section>
   </main>`)
